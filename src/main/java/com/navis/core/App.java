6085c630a73bd164e403f3b4e78f182c9b970cef
srinisa <sai.vignesh.srinivasan@navis.com>
2018-08-20 15:33:12 +0530

    my first commit

---
+++ src/main/java/com/navis/core/App.java
@@ -0,0 +1,19 @@
+package com.navis.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+public static void Main(String[] args){
+
+
+}
+    public void display(){
+
+        System.out.println("In Display");
+    }
+
+
+}
